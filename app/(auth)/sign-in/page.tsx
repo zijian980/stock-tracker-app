4d095e09e8fbfafc0ffe719dc96439621f514817
Zijian <163723805+zijian980@users.noreply.github.com>
2026-01-12 20:10:02 +0800

    Implemented search stocks

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -43,7 +43,7 @@ const SignIn = () => {
                 <InputField
                     name="email"
                     label="Email"
-                    placeholder="contact@jsmastery.com"
+                    placeholder="John@gmail.com"
                     register={register}
                     error={errors.email}
                     validation={{ required: 'Email is required', pattern: /^\w+@\w+\.\w+$/ }}
